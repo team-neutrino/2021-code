3ae7fef0d0cf75b8457304cffdbbc05071eb93b9
gurellia53 <gurellia53@gmail.com>
2021-02-16 19:21:09 -0800

    path is almost done

--- 2021-code/src/main/java/frc/robot/commands/BounceAuto.java
+++ 2021-code/src/main/java/frc/robot/commands/BounceAuto.java
@@ -25,6 +25,10 @@ public class BounceAuto extends SequentialCommandGroup {
     m_Drive = p_Drive;
     Trajectory m_bounce0 = ExampleTrajectory.bounce0;
     Trajectory m_bounce1 = ExampleTrajectory.bounce1;
+    Trajectory m_bounce2 = ExampleTrajectory.bounce2;
+    Trajectory m_bounce3 = ExampleTrajectory.bounce3;
+    Trajectory m_bounce4 = ExampleTrajectory.bounce4;
+    Trajectory m_bounce5 = ExampleTrajectory.bounce5;
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     RamseteCommand bounce0Command = new RamseteCommand(
@@ -57,6 +61,66 @@ public class BounceAuto extends SequentialCommandGroup {
         m_Drive::tankDriveVolts,
         m_Drive
     );
-    addCommands(bounce0Command, bounce1Command, new InstantCommand(() -> m_Drive.tankDriveVolts(0, 0)));
+    RamseteCommand bounce2Command = new RamseteCommand(
+        m_bounce2,
+        m_Drive::getPose,
+        new RamseteController(Constants.DriveConstants.K_RAMSETE_B, Constants.DriveConstants.K_RAMSETE_ZETA),
+        new SimpleMotorFeedforward(Constants.DriveConstants.KS_VOLTS,
+                                Constants.DriveConstants.KV_VOLT_SECONDS_PER_METER,
+                                Constants.DriveConstants.KA_VOLT_SECONDS_SQUARED_PER_METER),
+        Constants.DriveConstants.K_DRIVE_KINEMATICS,
+        m_Drive::getWheelSpeeds,
+        new PIDController(Constants.DriveConstants.KP_DRIVE_VEL, 0, 0),
+        new PIDController(Constants.DriveConstants.KP_DRIVE_VEL, 0, 0),
+  
+        m_Drive::tankDriveVolts,
+        m_Drive
+    );
+    RamseteCommand bounce3Command = new RamseteCommand(
+        m_bounce3,
+        m_Drive::getPose,
+        new RamseteController(Constants.DriveConstants.K_RAMSETE_B, Constants.DriveConstants.K_RAMSETE_ZETA),
+        new SimpleMotorFeedforward(Constants.DriveConstants.KS_VOLTS,
+                                Constants.DriveConstants.KV_VOLT_SECONDS_PER_METER,
+                                Constants.DriveConstants.KA_VOLT_SECONDS_SQUARED_PER_METER),
+        Constants.DriveConstants.K_DRIVE_KINEMATICS,
+        m_Drive::getWheelSpeeds,
+        new PIDController(Constants.DriveConstants.KP_DRIVE_VEL, 0, 0),
+        new PIDController(Constants.DriveConstants.KP_DRIVE_VEL, 0, 0),
+  
+        m_Drive::tankDriveVolts,
+        m_Drive
+    );
+    RamseteCommand bounce4Command = new RamseteCommand(
+        m_bounce4,
+        m_Drive::getPose,
+        new RamseteController(Constants.DriveConstants.K_RAMSETE_B, Constants.DriveConstants.K_RAMSETE_ZETA),
+        new SimpleMotorFeedforward(Constants.DriveConstants.KS_VOLTS,
+                                Constants.DriveConstants.KV_VOLT_SECONDS_PER_METER,
+                                Constants.DriveConstants.KA_VOLT_SECONDS_SQUARED_PER_METER),
+        Constants.DriveConstants.K_DRIVE_KINEMATICS,
+        m_Drive::getWheelSpeeds,
+        new PIDController(Constants.DriveConstants.KP_DRIVE_VEL, 0, 0),
+        new PIDController(Constants.DriveConstants.KP_DRIVE_VEL, 0, 0),
+  
+        m_Drive::tankDriveVolts,
+        m_Drive
+    );
+    RamseteCommand bounce5Command = new RamseteCommand(
+        m_bounce5,
+        m_Drive::getPose,
+        new RamseteController(Constants.DriveConstants.K_RAMSETE_B, Constants.DriveConstants.K_RAMSETE_ZETA),
+        new SimpleMotorFeedforward(Constants.DriveConstants.KS_VOLTS,
+                                Constants.DriveConstants.KV_VOLT_SECONDS_PER_METER,
+                                Constants.DriveConstants.KA_VOLT_SECONDS_SQUARED_PER_METER),
+        Constants.DriveConstants.K_DRIVE_KINEMATICS,
+        m_Drive::getWheelSpeeds,
+        new PIDController(Constants.DriveConstants.KP_DRIVE_VEL, 0, 0),
+        new PIDController(Constants.DriveConstants.KP_DRIVE_VEL, 0, 0),
+  
+        m_Drive::tankDriveVolts,
+        m_Drive
+    );
+    addCommands(bounce0Command, bounce1Command, bounce2Command, bounce3Command, bounce4Command, bounce5Command, new InstantCommand(() -> m_Drive.tankDriveVolts(0, 0)));
   }
 }
