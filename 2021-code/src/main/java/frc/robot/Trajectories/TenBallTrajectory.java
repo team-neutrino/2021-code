fad9846d708722d179b3c56d1351004c83c177a6
davidd23 <59034876+davidd23@users.noreply.github.com>
2021-03-01 19:57:28 -0600

    a

--- 2021-code/src/main/java/frc/robot/Trajectories/TenBallTrajectory.java
+++ 2021-code/src/main/java/frc/robot/Trajectories/TenBallTrajectory.java
@@ -19,17 +19,13 @@ public class TenBallTrajectory
 
     public static final Trajectory tenBallHalf = TrajectoryGenerator.generateTrajectory(
         List.of(new Pose2d(4.6, 0, Rotation2d.fromDegrees(-90)), new Pose2d(4, -2.6, Rotation2d.fromDegrees(-147))),
-        NeutrinoTrajectoryConfigs.m_FastForwardConfig);
+        NeutrinoTrajectoryConfigs.m_TwixtForwardConfig);
 
     public static final Trajectory tenBall1 = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(4, -2.6, Rotation2d.fromDegrees(-147)),
-        List.of(new Translation2d(2.7, -3.9)),
-        new Pose2d(4.3, -3.6, Rotation2d.fromDegrees(25)),
-        NeutrinoTrajectoryConfigs.m_FastForwardConfig);
+        new Pose2d(4, -2.6, Rotation2d.fromDegrees(-147)), List.of(new Translation2d(2.7, -3.9)),
+        new Pose2d(4.3, -3.6, Rotation2d.fromDegrees(25)), NeutrinoTrajectoryConfigs.m_SlowForwardConfig);
 
     public static final Trajectory tenBall2 = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(4.3, -3.76, Rotation2d.fromDegrees(25)),
-        List.of(new Translation2d(3.7, -5.5)),
-        new Pose2d(3.7, -6.4, Rotation2d.fromDegrees(-90)),
-        NeutrinoTrajectoryConfigs.m_FastForwardConfig);
+        new Pose2d(4.3, -3.76, Rotation2d.fromDegrees(25)), List.of(new Translation2d(3.7, -5.5)),
+        new Pose2d(3.7, -6.4, Rotation2d.fromDegrees(-90)), NeutrinoTrajectoryConfigs.m_SlowForwardConfig);
 }
