20fae922f4ce1ecc9178334151eb51cd89446644
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2021-03-08 18:32:40 -0600

    a

---
+++ 2021-code/src/main/java/frc/robot/util/ShooterButtons.java
@@ -0,0 +1,10 @@
+package frc.robot.util;
+
+public class ShooterButtons {
+    private int green = 62500;
+    private int blue = 65000;
+    private int red = 69000;
+    private int yellow = 61000;
+
+    
+}
