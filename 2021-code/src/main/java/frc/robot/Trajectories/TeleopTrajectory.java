e00c02e4e8e92d9cf96a1f8236fcbceadcefe6b3
davidd23 <77417861+ddavidd23@users.noreply.github.com>
2021-03-08 18:41:25 -0600

    reverse config

--- 2021-code/src/main/java/frc/robot/Trajectories/TeleopTrajectory.java
+++ 2021-code/src/main/java/frc/robot/Trajectories/TeleopTrajectory.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import frc.robot.NeutrinoTrajectoryConfigs;
@@ -16,5 +15,5 @@ public class TeleopTrajectory {
 
     public static final Trajectory fromPort = TrajectoryGenerator.generateTrajectory(
         List.of(new Pose2d(2, 0, new Rotation2d(0)), new Pose2d(0, 0, Rotation2d.fromDegrees(0))),
-        NeutrinoTrajectoryConfigs.m_FastForwardConfig);
+        NeutrinoTrajectoryConfigs.m_FastReverseConfig);
 }
