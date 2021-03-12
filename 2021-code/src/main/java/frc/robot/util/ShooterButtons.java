537abd8f2d955e1351d4f45cd1c79ffed1a53abb
gurellia53 <gurellia53@gmail.com>
2021-03-11 17:41:48 -0800

    Cleanup work moved constants from shooterbuttons,java to constants.java

--- 2021-code/src/main/java/frc/robot/util/ShooterButtons.java
+++ 2021-code/src/main/java/frc/robot/util/ShooterButtons.java
@@ -1,7 +1,6 @@
 
 package frc.robot.util;
 
-import static java.lang.Math.*;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -10,16 +9,14 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ShooterSetSpeedCommand;
 import frc.robot.subsystems.ShooterSubsystem;
 import static edu.wpi.first.wpilibj.XboxController.Button;
+
+import frc.robot.Constants;
 import frc.robot.Constants.*;
 
 public class ShooterButtons
 {
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 
-    private int green = 62500;
-    private int blue = 65000;
-    private int red = 69000;
-    private int yellow = 61000;
     private NetworkTableEntry angle_tY = table.getEntry("ty");
     private final double angle2 = 23;
     private final double limeLightHeight = 38;
@@ -29,9 +26,7 @@ public class ShooterButtons
     private XboxController m_OperatorController = new XboxController(ControllerPorts.XBOX_CONTROLLER_PORT);
 
     private JoystickButton m_A = new JoystickButton(m_OperatorController, Button.kA.value);
-    private JoystickButton m_B = new JoystickButton(m_OperatorController, Button.kB.value);
-    private JoystickButton m_X = new JoystickButton(m_OperatorController, Button.kX.value);
-    private JoystickButton m_Y = new JoystickButton(m_OperatorController, Button.kY.value);
+
     private final ShooterSubsystem m_Shooter = new ShooterSubsystem();
 
     public void ShooterButtons()
@@ -47,19 +42,19 @@ public class ShooterButtons
         double distance = finalHeight / tan;
         if (distance < 90)
         {
-            m_A.whenHeld(new ShooterSetSpeedCommand(m_Shooter, green));
+            m_A.whenHeld(new ShooterSetSpeedCommand(m_Shooter, Constants.ShooterConstants.green));
         }
         else if (distance > 90 && distance < 150)
         {
-            m_A.whenHeld(new ShooterSetSpeedCommand(m_Shooter, yellow));
+            m_A.whenHeld(new ShooterSetSpeedCommand(m_Shooter, Constants.ShooterConstants.yellow));
         }
         else if (distance > 150 && distance < 210)
         {
-            m_A.whenHeld(new ShooterSetSpeedCommand(m_Shooter, blue));
+            m_A.whenHeld(new ShooterSetSpeedCommand(m_Shooter, Constants.ShooterConstants.blue));
         }
         else if (distance > 210 && distance < 270)
         {
-            m_A.whenHeld(new ShooterSetSpeedCommand(m_Shooter, red));
+            m_A.whenHeld(new ShooterSetSpeedCommand(m_Shooter, Constants.ShooterConstants.red));
         }
 
         //System.out.println("Distance " + distance + " " + tan + " " + finalAngle );
