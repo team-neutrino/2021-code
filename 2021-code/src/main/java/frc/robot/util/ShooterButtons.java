22af52f9046a07c480defdb5d22ff13b508aae53
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2021-03-13 15:26:02 -0600

    d

--- 2021-code/src/main/java/frc/robot/util/ShooterButtons.java
+++ 2021-code/src/main/java/frc/robot/util/ShooterButtons.java
@@ -4,16 +4,9 @@ package frc.robot.util;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.ShooterSetSpeedCommand;
-import frc.robot.subsystems.HopperSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
-import static edu.wpi.first.wpilibj.XboxController.Button;
 
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
-import frc.robot.Constants.*;
 
 public class ShooterButtons
 {
@@ -26,14 +19,12 @@ public class ShooterButtons
     private final double finalHeight = powerPortHeight - limeLightHeight;
     private double getTY;
 
-    private final ShooterSubsystem m_Shooter = new ShooterSubsystem();
-
     public void ShooterButtons()
     {
     
     }
 
-    public int getShooterSpeed(JoystickButton m_B)
+    public int getDistance(JoystickButton m_B)
     {
         getTY = angle_tY.getDouble(0.0);
         double finalAngle = getTY + angle2;
@@ -41,28 +32,20 @@ public class ShooterButtons
         double distance = finalHeight / tan;
         if (distance < 90)
         {
-            System.out.println("green");
             return Constants.ShooterConstants.green;
         }
         else if (distance > 90 && distance < 150)
         {
-            System.out.println("yellow");
-
             return Constants.ShooterConstants.yellow;
         }
         else if (distance > 150 && distance < 210)
         {
-            System.out.println("blue");
-
             return Constants.ShooterConstants.blue;
         }
         else if (distance > 210 && distance < 270)
         {
-            System.out.println("red");
             return Constants.ShooterConstants.red;
         }
         return 0;
-
-        //System.out.println("Distance " + distance + " " + tan + " " + finalAngle );
     }
 }
