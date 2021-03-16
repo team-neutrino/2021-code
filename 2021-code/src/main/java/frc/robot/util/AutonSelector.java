53d60092595ff1475194af0c7bc62686d33031ec
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2021-03-16 18:20:45 -0500

    d

--- 2021-code/src/main/java/frc/robot/util/AutonSelector.java
+++ 2021-code/src/main/java/frc/robot/util/AutonSelector.java
@@ -6,9 +6,14 @@ package frc.robot.util;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.commands.RamseteGenCommand;
+import frc.robot.commands.Trajectories.BounceAuton;
+import frc.robot.commands.Trajectories.GalBlueA;
+import frc.robot.commands.Trajectories.GalRedA;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakePIDSubsystem;
 
 /** Add your docs here. */
 public class AutonSelector
@@ -17,6 +22,10 @@ public class AutonSelector
     private AnalogPotentiometer analogPot = new AnalogPotentiometer(input, 511.77);
     private final DriveSubsystem m_Drive = new DriveSubsystem();
     private RamseteGenCommand m_RamseteGen;
+    private DriveSubsystem p_Drive;
+    private IntakePIDSubsystem p_Intake; 
+    private GalBlueA m_BlueA = new GalBlueA(p_Drive, p_Intake);
+    private GalRedA m_RedA = new GalRedA(p_Drive, p_Intake);
     public AutonSelector()
     {
         /*
@@ -25,16 +34,19 @@ public class AutonSelector
          * m_Hopper, m_Drive, 10); m_EightBallAuto = new EightBallAuto(m_Shooter, m_Hopper, m_Intake, m_Drive,
          * m_Turret);
          */
-        // System.out.println(analogPot.get());
-        // if (140 < analogPot.get() && analogPot.get() < 160)
-        // {
-        //     m_RamseteGen = new RamseteGenCommand(m_Drive, Constants.PathConstants.GALACTIC_RED_A_PATH);
-        // }
-        // else
-        // {
-        //     m_RamseteGen = new RamseteGenCommand(m_Drive, Constants.PathConstants.GALACTIC_BLUE_A_PATH);
+        System.out.println(analogPot.get());
+        
+    }
 
-        // }
+    public Command getAutonCommand() {
+        if (140 < analogPot.get() && analogPot.get() < 160)
+        {
+            return m_RedA;
+        }
+        else
+        {
+            return m_BlueA;
+        }
     }
 
     public void Periodic()
