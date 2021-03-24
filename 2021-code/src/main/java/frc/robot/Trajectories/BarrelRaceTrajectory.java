e2cf41be92756bded4f06700466e7453dee9fb1a
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2021-03-24 18:11:16 -0500

    now works at 3.0 speed

--- 2021-code/src/main/java/frc/robot/Trajectories/BarrelRaceTrajectory.java
+++ 2021-code/src/main/java/frc/robot/Trajectories/BarrelRaceTrajectory.java
@@ -12,12 +12,12 @@ import frc.robot.NeutrinoTrajectoryConfigs;
 public class BarrelRaceTrajectory {
     public static final Trajectory barrelRace0 = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)),
     List.of(new Translation2d(3, 0), new Translation2d(3.4, -1.5), new Translation2d(2.3, -1.3), new Translation2d(2.2, -.3),
-    new Translation2d(4.5, -0.5), new Translation2d(6, 0.5), 
+    new Translation2d(4.5, -0.75), new Translation2d(6, 0.28), 
     //2nd curve`
     new Translation2d(5.9, 1.55), new Translation2d(4.8, 1.55), 
     new Translation2d(4.7, 0),  
     //3rd curve
-    new Translation2d(6.72, -0.8), new Translation2d(6.72, 0.4), new Translation2d(4.5, -0.5),
+    new Translation2d(6.9, -0.8), new Translation2d(6.9, 0.84),new Translation2d(6, -0.4), new Translation2d(4.5, -0.2),
     new Translation2d(1, -.2)),
     //endpoint
     new Pose2d(.8, -.2, Rotation2d.fromDegrees(180)),
