224ccd8fb884a621c71c897a1db726d4cfb8feb4
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2021-03-25 20:25:50 -0500

    got stuff working

--- 2021-code/src/main/java/frc/robot/Trajectories/SlalomTrajectory.java
+++ 2021-code/src/main/java/frc/robot/Trajectories/SlalomTrajectory.java
@@ -21,19 +21,19 @@ public class SlalomTrajectory
     public static final Trajectory slalom0 = TrajectoryGenerator.generateTrajectory(
         new Pose2d(0, 0, new Rotation2d(0)),
         List.of(
-            new Translation2d(1.2, 0.93), // first intersection
-            new Translation2d(2.35, 1.9), // top 1st
-            new Translation2d(5, 1.9), // top 2nd
+            new Translation2d(1.1, 1.2), // first intersection
+            new Translation2d(2.35, 1.5), // top 1st
+            new Translation2d(5, 1.6), // top 2nd
             //new Translation2d(6.4, 0.57), // 2nd intersection, 1st time
-            new Translation2d(6.7, 0), // circle bottom, y might need to b 0?
-            new Translation2d(7.7, 0.5), // circle right
-            new Translation2d(7.2, 1.8), // circle top
-            new Translation2d(6.4, 0.6), //2nd intersection, 2nd time
-            new Translation2d(5.2, -0.4), //bottom 1st pt
-            new Translation2d(4.6, -.1), //when it goes out of bounds, bottom middle
+            new Translation2d(6.5, 0), // circle bottom, y might need to b 0?
+            new Translation2d(7.4, 0.5), // circle right
+            new Translation2d(6.9, 1.8), // circle top
+            new Translation2d(6.5, -0.7), //2nd intersection, 2nd time
+            //new Translation2d(5.2, -0.9), //bottom 1st pt
+            new Translation2d(4.6, -.7), //when it goes out of bounds, bottom middle
             new Translation2d(2.2, 0), //bottom 2nd pt
-            new Translation2d(1.6, 0.7) //1st interesection 2nd time
+            new Translation2d(1.55, 1.2) //1st interesection 2nd time
             ), 
-        new Pose2d(0.6, 1.65, Rotation2d.fromDegrees(180)),
-        NeutrinoTrajectoryConfigs.m_FastForwardConfig);
+        new Pose2d(0.7, 1.6, Rotation2d.fromDegrees(180)),
+        NeutrinoTrajectoryConfigs.m_SlalomConfig);
 }
