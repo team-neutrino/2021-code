4761fb424831a9e63c1819c939aedde82715874e
Andrew Gurik <gurellia53@gmail.com>
2021-03-29 22:46:13 -0500

    update values

--- 2021-code/src/main/java/frc/robot/Constants.java
+++ 2021-code/src/main/java/frc/robot/Constants.java
@@ -87,10 +87,10 @@ public final class Constants
         public static final double WHEEL_D = 2;
         public static final double WHEEL_F = 0.008;
         public static final double SHOOTER_CONVERSION = 0;
-        public static final int green = 68000;
-        public static final int blue = 65000;
-        public static final int red = 69000;
-        public static final int yellow = 61000;
+        public static final int green = 69500;
+        public static final int blue = 66000;
+        public static final int red = 69100;
+        public static final int yellow = 60000;
     }
 
     public static final class HoodConstants
